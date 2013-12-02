288c2c88fd87cd7cac463a103f219a94343c21e9
HIYAMA Manabu <hiyama.manabu@po.ntts.co.jp>
2013-12-02 18:49:16 +0900

    test-dpi: Add test for DpiStatesController

--- ryu/dpi/test_ipv6.py
+++ ryu/dpi/test_ipv6.py
@@ -7,7 +7,11 @@ from nose.tools import *
 
 from ryu.lib import ofctl_v1_3
 from ryu.ofproto import ofproto_v1_3, ofproto_v1_3_parser
+from ryu.app.wsgi import ControllerBase, WSGIApplication, route
+from webob.response import Response
+from webob.request import Request
 from ryu.dpi import ipv6sw
+from ryu.controller.dpset import EventDP
 
 LOG = logging.getLogger(__name__)
 
@@ -16,11 +20,27 @@ class _Datapath(object):
     ofproto = ofproto_v1_3
     ofproto_parser = ofproto_v1_3_parser
 
-    def __init__(self):
+    def __init__(self, id=1):
         self.msgs = []
+        self.id = id
 
     def send_msg(self, msg):
         self.msgs.append(msg)
+
+
+class _DPSet(object):
+    def __init__(self):
+        self.dps = {}
+
+    def register(self, dp):
+         self.dps[dp.id] = dp
+         self.ev = EventDP(dp, True)
+
+    def get(self, dp_id):
+        return self.dps.get(dp_id)
+
+    def get_all(self):
+        return self.dps.items()
 
 
 class Test_ipv6sw(unittest.TestCase):
@@ -82,3 +102,65 @@ class Test_ipv6sw(unittest.TestCase):
             ok_(isinstance(msg, dp.ofproto_parser.OFPFlowMod))
             eq_(msg.command, dp.ofproto.OFPFC_DELETE)
             eq_(msg.cookie, cookies[i])
+
+class TestDpiStatsController(unittest.TestCase):
+
+    """ Test case for StatsController
+    """
+
+    def setUp(self):
+        dp = _Datapath()
+        dpset = _DPSet()
+        dpset.register(dp)
+
+        self.dpset = dpset
+        self.waiters = {}
+        self.data = {
+            'waiters': self.waiters,
+            'dpset': self.dpset,
+            'DpiRestApi': None,
+            'flow_list': ipv6sw.IPV6DPI
+        }
+        self.wsgi = WSGIApplication()
+        self.wsgi.register(ipv6sw.DpiStatsController, self.data)
+        #LOG.debug((self.data.keys()))
+        #LOG.debug((self.wsgi))
+
+    def tearDown(self):
+        pass
+
+    def _request_dpi(self, uri, code=200, method='GET', body=''):
+        # Todo: status Http library to use
+        status = { 200: '200 OK',
+                   400: '400 Bad Request',
+                   404: '404 Not Found',
+                   500: '500'}
+
+        req = Request.blank(uri)
+        req.method = method
+        req.body = body
+        return  self.wsgi(req.environ, lambda s, _: eq_(s, status[code]))
+
+    def test_dpi_received_404_unreserved_uri(self):
+        res = self._request_dpi('/dpi', 404)
+
+    def test_dpi_received_404_unreserved_method(self):
+        res = self._request_dpi('/dpi/flow', 404)
+
+    def test_dpi_received_400_body_is_none(self):
+        res = self._request_dpi('/dpi/flow', 400, 'PUT')
+        LOG.debug(("return", res[0], type(res[0])))
+
+        # Todo:
+        # res = ["{'body': '', 'err_msg': 'invalid syntax at body'}"]
+        #eq_(res[0].body, '')
+
+    def test_dpi_received_400_body_has_not_dpi(self):
+        body = "{'test': 0}"
+        res = self._request_dpi('/dpi/flow', 400, 'PUT', body)
+        LOG.debug(("return", res[0]))
+
+    def test_dpi_received_400_dpi_value_is_fail(self):
+        body = "{'dpi': 0}"
+        res = self._request_dpi('/dpi/flow', 400, 'PUT', body)
+        LOG.debug(("return", res[0]))

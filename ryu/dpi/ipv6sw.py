ac1e052903c21889a83fa460b65bf47ead82652a
HIYAMA Manabu <hiyama.manabu@po.ntts.co.jp>
2013-12-10 17:42:56 +0900

    dpi: Fix DPI_REST_LIST and log-messages

--- ryu/dpi/ipv6sw.py
+++ ryu/dpi/ipv6sw.py
@@ -64,12 +64,16 @@ def mod_flow_entry(dp, flow, cmd):
 
 
 def add_flows(dp, flows):
+    LOG.info("<------------------- Flow-Mod(ADD) [dp=%s, flows=%s]",
+             dp.id, len(flows))
     cmd = dp.ofproto.OFPFC_ADD
     for flow in flows:
         mod_flow_entry(dp, flow, cmd)
 
 
 def del_flows(dp, flows):
+    LOG.info("<---------------- Flow-Mod(DELETE) [dp=%s, flows=%s]",
+             dp.id, len(flows))
     cmd = dp.ofproto.OFPFC_DELETE
     for flow in flows:
         mod_flow_entry(dp, flow, cmd)
@@ -88,10 +92,8 @@ def wait_barrier(dp, waiters):
     waiters_per_dp = waiters.setdefault(dp.id, {})
     event = hub.Event()
     waiters_per_dp[barrier.xid] = event
-    LOG.info("<----------------- Barrier Request")
-    LOG.debug("dpid=%s xid=%s timer=%s",
-              dp.id, barrier.xid, BARRIER_REPLY_TIMER)
-    LOG.debug("----------------------------------")
+    LOG.info("<----------------- Barrier Request [dp=%s, xid=%s, timer=%s]",
+             dp.id, barrier.xid, BARRIER_REPLY_TIMER)
     dp.send_msg(barrier)
 
     ret = event.wait(timeout=BARRIER_REPLY_TIMER)
@@ -157,22 +159,14 @@ class DpiStatsController(StatsController):
     """
     DpiStatsController
     """
-    DPI_REST_LIST = {"dpi": ["on", "off"]}
+    DPI_REST_LIST = {"on": add_flows, "off": del_flows}
 
     def __init__(self, req, link, data, **config):
         super(DpiStatsController, self).__init__(req, link, data, **config)
         self.dpiflow = data["dpiflow"]
 
     def _dpi_flows_cmd(self, dp, flows, cmd):
-        if cmd == "on":
-            LOG.info("<------------------- Flow-Mod(ADD)")
-            add_flows(dp, flows)
-        elif cmd == "off":
-            LOG.info("<---------------- Flow-Mod(DELETE)")
-            del_flows(dp, flows)
-
-        LOG.debug("dpid=%s flows=%s", dp.id, len(flows))
-        LOG.debug("----------------------------------")
+        self.DPI_REST_LIST[cmd](dp, flows)
         return wait_barrier(dp, self.waiters)
 
     def _dpi_response(self, status, body, err_msg=None):
@@ -181,6 +175,8 @@ class DpiStatsController(StatsController):
             LOG.error("### REST-ERR: %s: %s", err_msg, str(body))
         else:
             _dpi_body = body
+
+        LOG.info("=============== Responded REST DPI")
         return webob.Response(content_type="application/json",
                               status=status, body=json.dumps(_dpi_body))
 
@@ -189,8 +185,7 @@ class DpiStatsController(StatsController):
         body = req.body
         LOG.info("================ Received REST DPI")
         LOG.info("Request_body=%s", body)
-        LOG.debug("dpset_dpids=%s", self.dpset.dps.keys())
-        LOG.debug("==================================")
+        LOG.debug("dpset_dps=%s", self.dpset.dps.keys())
 
         # REST Parameter check
         try:
@@ -199,7 +194,7 @@ class DpiStatsController(StatsController):
             err_msg = "invalid syntax at body"
             return self._dpi_response(400, body, err_msg)
 
-        if dpi_cmd not in self.DPI_REST_LIST["dpi"]:
+        if dpi_cmd not in self.DPI_REST_LIST:
             err_msg = "invalid value at dpi"
             return self._dpi_response(400, body, err_msg)
 
@@ -208,15 +203,15 @@ class DpiStatsController(StatsController):
         flow_list = self.dpiflow[flow_tag]
         dpid = flow_list.check_dp(self.dpset)
         if dpid is not None:
-            err_msg = "Datapath[dpid=%s] is None" % dpid
+            err_msg = "Datapath[dp=%s] is None" % dpid
             return self._dpi_response(500, body, err_msg)
 
         # FlowMod & BariierRequest
         for dpid, flows in flow_list.get_items():
             dp = self.dpset.get(dpid)
-            LOG.debug("FlowMod dpid=%s flows=%s", dp.id, flow_tag)
+            LOG.debug("FlowMod dp=%s flows=%s", dp.id, flow_tag)
             if not self._dpi_flows_cmd(dp, flows, dpi_cmd):
-                err_msg = "BarrierRequest Timeout. [dpid=%s]" % dp.id
+                err_msg = "BarrierRequest Timeout. [dp=%s]" % dp.id
                 return self._dpi_response(500, body, err_msg)
 
         return self._dpi_response(200, body)
@@ -257,10 +252,10 @@ class DpiRestApi(RestStatsApi):
         msg = ev.msg
         dp = msg.datapath
         LOG.info("==================> SwitchFeatures")
-        LOG.info("dpid=%s, dpset=%s", dp.id, self.dpset.get_all())
+        LOG.info("dp=%s, dpset=%s", dp.id, self.dpset.get_all())
 
         flows = [FLOW_PKT_IN]
-        LOG.debug("FlowMod dpid=%s flows=PacketIn", dp.id)
+        LOG.debug("FlowMod dp=%s flows=PacketIn", dp.id)
         add_flows(dp, flows)
 
         LOG.debug("==================================")
@@ -270,12 +265,12 @@ class DpiRestApi(RestStatsApi):
         if ev.enter:
             dp = ev.dp
             LOG.info("===================> dpset EventDP")
-            LOG.info("dpid=%s, xid=%s, dpset=%s",
+            LOG.info("dp=%s, xid=%s, dpset=%s",
                      dp.id, dp.xid, self.dpset.dps.keys())
 
             flow_tag = DPI_FLOWLIST["default"]
             flows = self.dpiflow[flow_tag].get_flows(dp.id)
-            LOG.debug("FlowMod dpid=%s flows=%s", dp.id, flow_tag)
+            LOG.debug("FlowMod dp=%s flows=%s", dp.id, flow_tag)
             add_flows(dp, flows)
             LOG.debug("==================================")
 
@@ -283,9 +278,8 @@ class DpiRestApi(RestStatsApi):
     def _barrier_reply_handler(self, ev):
         msg = ev.msg
         dp = msg.datapath
-        LOG.info("-------------------> Barrier Reply")
-        LOG.debug("dpid=%s, msg=%s", dp.id, msg)
-        LOG.debug("----------------------------------")
+        LOG.info("-------------------> Barrier Reply [dp=%s, xid=%s, ofp=%s]",
+                 dp.id, msg.xid, msg.version)
 
         if (dp.id not in self.waiters
                 or msg.xid not in self.waiters[dp.id]):
@@ -302,6 +296,6 @@ class DpiRestApi(RestStatsApi):
         in_port = msg.match["in_port"]
         pkt = packet.Packet(msg.data)
         LOG.info("=======================> Packet-in")
-        LOG.info("dpid=%s, in_port=%s", dp.id, in_port)
+        LOG.info("dp=%s, in_port=%s", dp.id, in_port)
         LOG.debug("packet=%s", str(pkt))
         LOG.debug("==================================")

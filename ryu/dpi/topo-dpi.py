3a7ad508c068c7c870918fe4c7194375cf237089
HIYAMA Manabu <hiyama.manabu@po.ntts.co.jp>
2014-01-28 18:16:31 +0900

    topo-dpi: add topology "dpi-w1"

--- ryu/dpi/topo-dpi.py
+++ ryu/dpi/topo-dpi.py
@@ -1,32 +1,48 @@
 """
 Custom topology for DPI
 
-                     s2                   w1
- (1)---(1)  (3)---(1)  (2)---(1)  (3)---(1)
-d1        s4                    s1
- (1)---(2)  (4)---(1)  (2)---(2)  (4)---(1)
-                     s3                   w2
+ startup
+ =======
+ $ sudo mn --controller remote --custom topo-dpi.py --pre mn-pre \
+     --topo [dpi|dpi-w1]
 
-d1(1)=d1-eth0: 10.0.0.1 / fe80::200:ff:fe00:1
-               2001:db8:2000::11
-               2001:db8:2000::111
-w1(1)=w1-eth0: 10.0.0.2 / fe80::200:ff:fe00:2
-               2001:db8:2000::13
-w2(1)=w2-eth0: 10.0.0.3 / fe80::200:ff:fe00:3
-               2001:db8:2000::14
 
-$ sudo mn --mac --controller remote --custom topo-dpi.py --topo dpi --pre mn-pre
+ topo=dpi: DPI and WebServer-2
+ =============================
+
+              (2)---(1)s2(2)---(1)  (3)---(1)w1
+ d1(1)---(1)s4                    s1
+              (3)---(1)s3(2)---(2)  (4)---(1)w2
+
+
+ topo=dpi-w1: DPI and WebServer-1
+ ================================
+
+              (2)---(1)s2(2)---(1)
+ d1(1)---(1)s4                    s1(3)---(1)w1
+              (3)---(1)s3(2)---(2)
+
+
+ host interfaces
+ ===============
+ d1(1)=d1-eth0: 10.0.0.1
+                fe80::200:ff:fe00:1
+                2001:db8:2000::11
+                2001:db8:2000::111
+ w1(1)=w1-eth0: 10.0.0.2
+                fe80::200:ff:fe00:2
+                2001:db8:2000::13
+ w2(1)=w2-eth0: 10.0.0.3
+                fe80::200:ff:fe00:3
+                2001:db8:2000::14
 
 """
 
 from mininet.topo import Topo
 
 
-class MyTopo(Topo):
-
+class DpiTopoWeb1(Topo):
     def __init__(self):
-        "Create custom topo."
-
         # Initialize topology
         Topo.__init__(self)
 
@@ -34,22 +50,31 @@ class MyTopo(Topo):
         opts = {"protocols": ["OpenFlow13"]}
 
         # Add hosts and switches
-        DPI1 = self.addHost('d1')
-        Edge4 = self.addSwitch('s4')
-        Bridge2 = self.addSwitch('s2')
-        Bridge3 = self.addSwitch('s3')
-        Edge1 = self.addSwitch('s1')
-        Web1 = self.addHost('w1')
-        Web2 = self.addHost('w2')
+        self.DPI1 = self.addHost('d1')
+        self.Edge4 = self.addSwitch('s4')
+        self.Bridge2 = self.addSwitch('s2')
+        self.Bridge3 = self.addSwitch('s3')
+        self.Edge1 = self.addSwitch('s1')
+        self.Web1 = self.addHost('w1')
 
         # Add links
-        self.addLink(Edge4, DPI1)
-        self.addLink(Edge4, Bridge2)
-        self.addLink(Edge4, Bridge3)
-        self.addLink(Edge1, Bridge2)
-        self.addLink(Edge1, Bridge3)
-        self.addLink(Edge1, Web1)
-        self.addLink(Edge1, Web2)
+        self.addLink(self.Edge4, self.DPI1)
+        self.addLink(self.Edge4, self.Bridge2)
+        self.addLink(self.Edge4, self.Bridge3)
+        self.addLink(self.Edge1, self.Bridge2)
+        self.addLink(self.Edge1, self.Bridge3)
+        self.addLink(self.Edge1, self.Web1)
 
 
-topos = {'dpi': (lambda: MyTopo())}
+class DpiTopoWeb2(DpiTopoWeb1):
+    def __init__(self):
+        DpiTopoWeb1.__init__(self)
+
+        # Add hosts and switches
+        self.Web2 = self.addHost('w2')
+
+        # Add links
+        self.addLink(self.Edge1, self.Web2)
+
+
+topos = {'dpi': (lambda: DpiTopoWeb2()), 'dpi-w1': (lambda: DpiTopoWeb1())}
